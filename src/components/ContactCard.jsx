a45f9ce337a03400ef6dcbdeff0a0461625c43d4
Gustavo Andres Santoyo Benavides <150487562+DocGus@users.noreply.github.com>
2025-05-06 14:27:55 +0000

    Terminado

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -2,19 +2,17 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 import useGlobalReducer from "../hooks/useGlobalReducer"
 function ContactCard(props) {
-    const{dispatch}=useGlobalReducer()
+    const { dispatch } = useGlobalReducer()
     async function deleteContact() {
         try {
             let response = await fetch(`https://playground.4geeks.com/contact/agendas/Gus/contacts/${props.contactou.id}`, {
                 method: "DELETE"
             })                                              //modo 2
-            if(!response.ok){
+            if (!response.ok) {
                 throw new Error("Error al eliminar contaco")
             }
-            const data=await response.json()
-            const action={type:"delete_contact", payload: props.contactou.id}
+            const action = { type: "delete_contact", payload: props.contactou.id }
             dispatch(action)
-            console.log(data)
         } catch (error) {
             console.error(error)
         }
@@ -39,10 +37,28 @@ function ContactCard(props) {
                 <div
                     className="col-md-2 d-flex flex-row align-items-start mt-2 gap-1">
                     <Link to={`/editar-contacto/${props.contactou.id}`} className="btn btn-outline-primary"><i className="fa-regular fa-pen-to-square"></i></Link>
-                    <button className="btn btn-outline-danger" onClick={deleteContact}><i className="fa-solid fa-trash-can"></i></button>
+                    <button className="btn btn-outline-danger"  data-bs-toggle="modal" data-bs-target="#modal"><i className="fa-solid fa-trash-can"></i></button>
+
+                    {/* <!-- Modal --> */}
+                    <div class="modal fade" id="modal" tabindex="-1">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Alerta</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                   ¿ Seguro que desea Eliminar Contacto ?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                                    <button type="button" class="btn btn-primary" onClick={deleteContact} data-bs-dismiss="modal">Eliminar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
 
-            
                 </div>
             </div>
         </div>
